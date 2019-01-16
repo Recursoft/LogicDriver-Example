ba61d1860df0f10666c4625703ac71e7f963b84d
Patrick Hardy <patshardy@gmail.com>
2019-01-15 19:27:44 -0700

    cleaned up more directories and pointed source code to point to new third person bp location

--- Source/SmExample/SmExampleGameMode.cpp
+++ Source/SmExample/SmExampleGameMode.cpp
@@ -7,7 +7,7 @@
 ASmExampleGameMode::ASmExampleGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
